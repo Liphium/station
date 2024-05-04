53fce5e8a0f52544cdd6b721c16eda7f9b92af53
Unbreathable <70802809+Unbreathable@users.noreply.github.com>
2024-05-04 19:49:09 +0200

    Added getting display name on /account/me endpoint

--- backend/routes/v1/account/me.go
+++ backend/routes/v1/account/me.go
@@ -3,8 +3,10 @@ package account
 import (
 	"github.com/Liphium/station/backend/database"
 	"github.com/Liphium/station/backend/entities/account"
+	"github.com/Liphium/station/backend/entities/account/properties"
 	"github.com/Liphium/station/backend/util"
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 )
 
 // Route: /account/me
@@ -24,6 +26,12 @@ func me(c *fiber.Ctx) error {
 		return util.FailedRequest(c, "server.error", err)
 	}
 
+	// Get account
+	var profile properties.Profile = properties.Profile{}
+	if err := database.DBConn.Where("id = ?", acc.ID).Take(&profile).Error; err != nil && err != gorm.ErrRecordNotFound {
+		return util.FailedRequest(c, "server.error", err)
+	}
+
 	// Get all valid permissions the account has
 	perms := []string{}
 	for name := range util.Permissions {
@@ -34,8 +42,9 @@ func me(c *fiber.Ctx) error {
 
 	// Retrun details
 	return util.ReturnJSON(c, fiber.Map{
-		"success":     true,
-		"account":     acc,
-		"permissions": perms,
+		"success":      true,
+		"account":      acc,
+		"permissions":  perms,
+		"display_name": profile.DisplayName,
 	})
 }
