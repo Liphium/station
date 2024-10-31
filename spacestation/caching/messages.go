a1fad3d00f11ff717e42121bf706dccb9d359799
Unbreathable <unfehlerhaftgmail.com>
2024-10-31 09:43:20 +0100

    Added an untested get messages function for spaces messaging

--- spacestation/caching/messages.go
+++ spacestation/caching/messages.go
@@ -4,6 +4,7 @@ import (
 	"slices"
 	"sync"
 
+	"github.com/Liphium/station/spacestation/util"
 	"github.com/google/uuid"
 )
 
@@ -19,12 +20,24 @@ type Message struct {
 
 type MessageSink struct {
 	Mutex    *sync.Mutex
-	Messages []Message
+	Messages []Message // A sorted list of messages
 }
+
+// How the messages should be sorted:
+// The newest messages should have the highest indices of the message slice.
+//
+// Index - Creation Time - Message
+// 0 - 10000 - "hello" (oldest message, at index 0)
+// 1 - 20000 - "wassup"
+// 2 - 30000 - "doing good?"
+// 3 - 40000 - "yessir" (newest message, at highest index)
 
 // Room id -> *MessageSink
 var messageMap *sync.Map = &sync.Map{}
 
+// Gets 10 messages before the specified time using an optimized binary search
+// algorithm to make sure this doesn't take long even when there are hundreds or
+// thousands of messages.
 func GetMessagesBefore(room string, time int64) ([]Message, error) {
 
 	// Get the message sink
@@ -34,19 +47,85 @@ func GetMessagesBefore(room string, time int64) ([]Message, error) {
 	}
 	sink := obj.(*MessageSink)
 
-	// Lock the mutex
+	// Copy the messages out of the sink
 	sink.Mutex.Lock()
-	defer sink.Mutex.Unlock()
+	messagesCopy := make([]Message, len(sink.Messages))
+	copy(messagesCopy, sink.Messages)
+	sink.Mutex.Unlock()
 
 	// If there aren't more than 10 messages just return them as is
-	if len(sink.Messages) <= 10 {
-		return sink.Messages, nil
+	if len(messagesCopy) <= 10 {
+
+		// Remove the messages that aren't before the specified time
+		return slices.DeleteFunc(messagesCopy, func(msg Message) bool {
+			return msg.Creation >= time
+		}), nil
 	}
 
-	// Get the first message before the passed in time
-	index, valid := slices.BinarySearchFunc(sink.Messages, time, func(msg Message, time int64) int {
-		return int(msg.Creation - time)
-	})
+	// Find the nearest message to the time
+	found := false
+	length := len(messagesCopy)
+	currentJump := length / 2
+	currentIndex := currentJump - 1
+	maxIndex := length - 1
+	for {
+		msg := messagesCopy[currentIndex]
 
-	return []Message{}, nil
+		// Check if the current message was sent before the specified time
+		if msg.Creation < time {
+
+			// If the index is equal to zero, the process is done (no more messages to search)
+			if currentIndex == 0 {
+				found = false
+				break
+			}
+
+			// If the index is at the top of the array, the process is done (all messages before time)
+			if currentIndex == maxIndex {
+				found = true
+				break
+			}
+
+			// If the message sent after the current message (at an index higher than the current message)
+			// is not below the time parameter, the current message is the start of the array
+			if messagesCopy[currentIndex+1].Creation >= time {
+				found = true
+				break
+			}
+
+			// If the message was before, but not enough, jump to a higher index
+			currentIndex = currentIndex + currentJump
+			if currentIndex > maxIndex {
+				currentIndex = maxIndex
+			}
+		} else {
+
+			// If it was not sent before the specified time, jump to a lower index
+			currentIndex = currentIndex - currentJump
+			if currentIndex < 0 {
+				currentIndex = 0
+			}
+		}
+
+		// Decrease the jump by half if possible
+		if currentJump <= 1 {
+			currentJump = 1
+		} else {
+			currentJump /= 2
+		}
+	}
+
+	// If no index has been found, return an empty list of messages
+	if !found {
+		return []Message{}, nil
+	}
+
+	// Get 10 messages before the current index
+	messages := make([]Message, 10)
+	copied := copy(messages, messagesCopy[currentIndex:])
+
+	// TODO: Remove this debug messages after testing
+	util.Log.Println("Copied ", copied, " messages into the array")
+
+	return messages, nil
 }
