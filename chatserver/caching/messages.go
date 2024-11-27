d754d0d995b2332af36989d838bb3403a5aadc6a
Unbreathable <unfehlerhaftgmail.com>
2024-11-27 09:31:50 +0100

    Message synchronization draft

--- chatserver/caching/messages.go
+++ chatserver/caching/messages.go
@@ -3,14 +3,19 @@ package caching
 import (
 	"sync"
 
+	"github.com/Liphium/station/chatserver/database"
+	"github.com/Liphium/station/chatserver/database/conversations"
 	"github.com/Liphium/station/main/integration"
+	"github.com/Liphium/station/pipes"
 )
 
 type SyncData struct {
+	TokenID      string
 	Conversation string
 	Since        int64
 }
 
+// The stuff needed to manage threads
 var mutexLock = &sync.Mutex{}
 var bufferedChannel chan struct{}
 
@@ -39,8 +44,40 @@ func AddSyncToQueue(data SyncData) error {
 	currentChan := bufferedChannel
 	currentChan <- struct{}{}
 
-	// Make space for a new thread
-	<-currentChan
+	// Make space for a new thread after work is finished
+	defer func() {
+		<-currentChan
+	}()
+
+	// Sync all messages until there are none left
+	finished := false
+	for !finished {
+
+		// Get the messages
+		var messages []conversations.Message
+		if err := database.DBConn.Where("creation >= ?", data.Since).Order("creation ASC").Limit(30).Find(&messages).Error; err != nil {
+			return err
+		}
+
+		// Send an event to the client containing the messages
+		if err := CSNode.Pipe(pipes.ProtocolWS, pipes.Message{
+			Local:   true,
+			Channel: pipes.BroadcastChannel([]string{"s-" + data.TokenID}),
+			Event: pipes.Event{
+				Name: "conv_msg_mp",
+				Data: map[string]interface{}{
+					"cv":   data.Conversation,
+					"msgs": messages,
+				},
+			},
+		}); err != nil {
+			return err
+		}
+
+		// Update state for the next iteration
+		finished = len(messages) < 30
+		data.Since = messages[len(messages)-1].Creation
+	}
 
 	return nil
 }
